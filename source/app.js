188c4c1d2ffd2f45b3a874f76085da439ae89601
mbriedelsperger <77255981+mbriedelsperger@users.noreply.github.com>
2021-08-15 12:34:48 -0500

    added javascript

---
+++ source/app.js
@@ -0,0 +1,57 @@
+let apiKey = "2e53852c75255f55216c821fb2c12211";
+let units="metric";
+let city="chicago";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
+
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
+
+function displayTemperature(response) {
+    let temperature = document.querySelector("#temperature");
+    let city = document.querySelector("#city");
+    let description= document.querySelector("#description");
+    let humidity = document.querySelector("#humidity");
+    let wind = document.querySelector("#wind");
+    let date = document.querySelector("#date");
+    let icon = document.querySelector("#icon");
+  
+    celsiusTemperature = response.data.main.temp;
+  
+    temperature.innerHTML = Math.round(celsiusTemperature);
+    city.innerHTML = response.data.name;
+    description.innerHTML = response.data.weather[0].description;
+    humidity.innerHTML = response.data.main.humidity;
+    wind.innerHTML = Math.round(response.data.wind.speed);
+    date.innerHTML = formatDate(response.data.dt * 1000);
+    icon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+    getForecast(response.data.coord);
+  }
+  
