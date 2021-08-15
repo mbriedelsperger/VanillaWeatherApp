9a7f4dd8b2e2d82bc7bbc4cd206239934171139a
mbriedelsperger <77255981+mbriedelsperger@users.noreply.github.com>
2021-08-15 13:33:45 -0500

    added the appropriate icon from the API

--- source/app.js
+++ source/app.js
@@ -50,7 +50,7 @@ function displayTemperature(response) {
       "src",
       `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    icon.setAttribute("alt", response.data.weather[0].description);
   
     getForecast(response.data.coord);
   }
